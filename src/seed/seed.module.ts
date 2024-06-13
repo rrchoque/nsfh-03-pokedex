e4389368c44966b8728d584b4fe4a5933f7f896b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-06-13 06:34:12 -0400

    Se implementó el patron adaptador en axios

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
-import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from 'src/pokemon/pokemon.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [PokemonModule, MongooseModule],
+  imports: [PokemonModule, CommonModule],
 })
 export class SeedModule {}
